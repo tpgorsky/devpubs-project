26c586ff5b84e1709c2d334732224eaae2a894a8
tpgorsky <bbibars8@gmail.com>
2026-07-02 21:46:00 +0500

    fix

--- blog/urls.py
+++ blog/urls.py
@@ -5,11 +5,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('', views.home_view, name='home'),
     path('posts/', views.post_list_view, name='post_list'),
     path('posts/<int:pk>/', views.post_detail_view, name='post_detail'),
     path('posts/new/', views.post_create_view, name='post_create'),
     path('posts/<int:pk>/edit/', views.post_edit_view, name='post_edit'),
-
     # Авторизация (встроенные Django views)
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
